75f415f3976585f0706f0dee6157793f4661b64b
Lucas Rocha <lr359508@gmail.com>
2024-04-25 13:01:01 -0300

    fix: alg2 otimizado e refatorado

--- funcs.c
+++ funcs.c
@@ -31,7 +31,7 @@ void alg1(char *img_name, char *dir, int n, int width, int height){
 
         if (!(sub_images[k].Data = (unsigned char *) calloc(width * height, sizeof(unsigned char))))
         {
-            printf("Faltou memória.");
+            printf("Falta de memória.\n");
             exit(1);
         }
 
@@ -49,10 +49,16 @@ void alg2(char *imagem, char *diretorio){
     struct Image src;
     readPGMImage(&src,imagem);
 
-    struct Image rec;
+    struct Image sub_image;
 
-    int *p = NULL;
-    double **v = NULL;
+    int p[2];
+
+    double *v = calloc(sub_image.height * sub_image.width, sizeof(double));
+    if (!v){
+        printf("Falta de memória.\n");
+        exit(1);
+    }
+
     double maior_corr = -INFINITY;
 
     char first_rec = 1;
@@ -60,82 +66,52 @@ void alg2(char *imagem, char *diretorio){
     DIR *d;
     struct dirent *dir;
 
-    p = calloc(2, sizeof(int));
-    if (!p){
-        printf("Falta de memória.\n");
-        exit(1);
-    }
-
     d = opendir(diretorio);
 
     FILE *file_ptr = fopen("alg2.txt","w");
 
     if(!(file_ptr)){
-        puts("ERRO.");
+        puts("Erro ao abrir criar o arquivo.");
         exit(1);
     }
 
-    if(d)
-    {
-        while ((dir = readdir(d)) != NULL)
-        {
-            if (strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0) {
-                continue;
-            }
-            
-            maior_corr = -INFINITY;
+    if(!d){
+        printf("Erro ao abrir o diretório '%s'.\n", diretorio);
+        exit(1);
+    }
 
-            char nome[400];
-            sprintf(nome,"%s/%s",diretorio,dir->d_name);
-            readPGMImage(&rec,nome);
-
-            if(first_rec){
-                v = calloc(rec.height, sizeof(double *));
-                if (!v){
-                    printf("Falta de memória.\n");
-                    exit(1);
-                }
-                for (int i = 0; i < rec.height; i++) {
-                    v[i] = calloc(rec.width, sizeof(double));
-                    if (!v[i]){
-                    printf("Falta de memória.\n");
-                    exit(1);
-                    }
-                }
-                first_rec = 0;
-            }
-
-            double media_rec = media_data(rec);
-            for (int a = 0; a < rec.height; a++) {
-                for (int b = 0; b < rec.width; b++) {
-                    v[a][b] = (double)rec.Data[a][b] / media_rec;
-                }
-            }
-
-            for (int i = 0; i < src.height - rec.height + 1; i++) {
-                for (int j = 0; j < src.width - rec.width + 1; j++) {
-                    double corr = correlacao_cruzada(src.Data, v, src.height, src.width, rec.height, rec.width, i, j);
-                    if (corr > maior_corr) {
-                        maior_corr = corr;
-                        p[0] = i;
-                        p[1] = j;
-                    }
-                }
-            }
-            
-            fprintf(file_ptr,"%s, %d, %d\n",dir->d_name,p[0],p[1]);
+    while ((dir = readdir(d)) != NULL){
+        if (strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0) {
+            continue;
         }
-        fclose(file_ptr);
-        closedir(d);
+        
+        maior_corr = -INFINITY;
+
+        char name[400];
+        sprintf(name,"%s/%s", diretorio, dir->d_name);
+        readPGMImage(&sub_image, name);
+
+        double sub_image_mean = media_data(sub_image);
+        for(int i = 0; i < sub_image.height * sub_image.width; i++){
+            v[i] = sub_image.Data[i] / sub_image_mean;
+        }
+
+        for (int i = 0; i < src.height - sub_image.height + 1; i++) {
+            for (int j = 0; j < src.width - sub_image.width + 1; j++) {
+                double corr = correlacao_cruzada(src.Data, v, src.height, src.width, sub_image.height, sub_image.width, i, j);
+                if (corr > maior_corr) {
+                    maior_corr = corr;
+                    p[0] = i;
+                    p[1] = j;
+                }
+            }
+        }
+        
+        fprintf(file_ptr, "%s, %d, %d\n", dir->d_name, p[0], p[1]);
     }
-    else{
-        printf("Erro ao abrir o diretório '%s'.\n",diretorio);
-        exit(1);
-    }
+    fclose(file_ptr);
+    closedir(d);
     
-    for (int i = 0; i < rec.height; i++) {
-        free(v[i]);
-    }
     free(v);
 }
 
@@ -159,7 +135,7 @@ struct Image filtro(struct Image obj){
     img.width = obj.width;
     img.maxval = obj.maxval;
     if (!(img.Data = (unsigned char *) calloc(img.width * img.height, sizeof(unsigned char)))){
-        printf("Faltou memória.");
+        printf("Falta de memória.\n");
         exit(1);
     }
 
@@ -170,7 +146,7 @@ struct Image filtro(struct Image obj){
     aux.height = obj.height + 2;
     aux.width = obj.height + 2;
     if (!(aux.Data = (unsigned char *) calloc(aux.width * aux.height, sizeof(unsigned char)))){
-        printf("Faltou memória.");
+        printf("Falta de memória.\n");
         exit(1);
     }
 
@@ -239,13 +215,11 @@ double correlacao_cruzada(unsigned char **src, double **rec, int src_height, int
     return soma;
 }
 
-double media_data(struct Image o){
-    double media=0;
-    for(int i=0;i<o.height;i++){
-        for(int j=0;j<o.width;j++){
-            media+=o.Data[i][j];
-        }
+double media_data(struct Image obj){
+    double media = 0;
+    for(int i = 0; i < obj.height * obj.width; i++){
+        media += obj.Data[i];
     }
-    media /= o.height*o.width;
+    media /= obj.height*obj.width;
     return media;
 }
